ae255eaad12178fd591f48616ff9aa8e3668b26a
israelsantana01 <israelofi2@gmail.com>
2021-04-06 16:02:04 -0300

    Continue refactoring code

--- issue02/answer.js
+++ issue02/answer.js
@@ -22,38 +22,49 @@ productKeys.forEach(productKey => {
   sectionMap[sectionKey].products[productKey] = { description };
 });
 
-sectionValues.forEach((section, i) => {
-  let groupKeys = Object.keys(section.groups);
-  let groupValues = Object.values(section.groups);
+// sectionValues.forEach((section, i) => {
+sectionKeys.forEach((sectionKey, i) => {
+  // let groupKeys = Object.keys(section.groups);
+  // let groupValues = Object.values(section.groups);
+  let group = sectionsJson[sectionKey];
+  let groupKeys = Object.keys(group);
 
-  if (groupValues.length > 1) {
-    groupValues.forEach((groupValue, j) => {
-      let key = `${sectionKeys[i]}_${groupKeys[j]}`;
+  // if (groupValues.length > 1) {
+  if (groupKeys.length > 1) {
+    // groupValues.forEach((groupValue, j) => {
+    groupKeys.forEach(groupKey => {
+      let key = `${sectionKey}_${group[groupKey]}`;
       let value = {
-        productName: groupValue.name,
-        sectionName: section.name
+        productName: group[groupKey].name,
+        sectionName: sectionsJson[sectionKey].name
       };
 
       groupMap[key] = value;
     });
   }
 
-  let key = `${sectionKeys[i]}_${groupKeys[0]}`;
+  let key = `${sectionKey}_${groupKeys[0]}`;
   let value = {
-    productName: groupValues[0].name,
-    sectionName: section.name
+    productName: group[groupKeys[0]].name,
+    sectionName: sectionsJson[sectionKey].name
   };
 
   groupMap[key] = value;
 });
 
-productValues.forEach((product, i) => {
-  let sectionName = sectionMap[product.sectionKey];
-  let sectionItem = sectionMap[product.sectionKey].products[productKeys[i]];
-  let group = groupMap[`${product.sectionKey}_${product.groupKey}`];
+console.log(groupMap);
 
-  sectionItem.description = `${group.productName} ${sectionItem.description}`;
-  sectionName.name = group.sectionName;
+// productValues.forEach((product, i) => {
+productKeys.forEach(productKey => {
+  let product = productsJson[productKey]
+
+  // let sectionName = sectionMap[product.sectionKey];
+  // let sectionItem = sectionMap[product.sectionKey].products[productKey];
+  let group = groupMap[`${product.sectionKey}_${product.groupKey}`];
+  console.log(group);
+
+  // sectionItem.description = `${group.productName} ${sectionItem.description}`;
+  // sectionName.name = group.sectionName;
 });
 
 fs.writeFileSync('answer.json', JSON.stringify(sectionMap, null, ' '));
